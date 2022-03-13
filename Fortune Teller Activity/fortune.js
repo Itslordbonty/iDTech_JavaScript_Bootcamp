f65c5042c8ab200d92fec6a254a8171544c31416
Itslordbonty <dman267321@gmail.com>
2022-03-13 13:56:25 +0800

    new file

---
+++ Fortune Teller Activity/fortune.js
@@ -0,0 +1,14 @@
+var fortune = 0;
+console.log("HELLO CITIZEN! YOUR FORTUNE IS " + fortune +"!")
+if (fortune <= 3){
+    console.log("Oooofff... Ouch, that's a low fortune. Better luck next time!")
+}
+else if (fortune >= 4 && fortune <= 7){
+    console.log("Hmm... That's still a pretty good fortune. Not the best, but if it ain't broke...")
+}
+else if (fortune >= 8 && fortune <= 10){
+    console.log("JEEEEEZZZZZ, MY GUY!!! YOU ARE ONE LUCKY PERSON!!")
+}
+else{
+    console.log("Looks like your fortune is a bit hard to read. Clear your mind, and let's try again.")
+}
